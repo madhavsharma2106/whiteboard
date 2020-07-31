fa09fd3c3e4dd012b30c87318aebfb43e08f48c3
madhavsharma2106 <madhavsharma2106@gmail.com>
2020-08-01 00:19:52 +0530

    Setup monaco

--- client/src/components/CodeShareRoom/monaco/Monaco.jsx
+++ client/src/components/CodeShareRoom/monaco/Monaco.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useRef } from "react";
+import React, { useState, useRef, useEffect } from "react";
 import { ControlledEditor } from "@monaco-editor/react";
 import { initialValue } from "../../../utils";
 import { Editor } from "./Editor";
@@ -19,11 +19,21 @@ function Monaco() {
   const [isEditorReady, setIsEditorReady] = useState(false);
   const [editor, setEditor] = useState(null);
   const editorRef = useRef(null);
+
   function handleEditorDidMount(_, editor) {
     setIsEditorReady(true);
     editorRef.current = editor;
     setEditor(new Editor(editorRef));
   }
+
+  const initialiseListeners = () => {};
+
+  useEffect(() => {
+    if (editorRef && isEditorReady) {
+      initialiseListeners();
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [editorRef, isEditorReady]);
 
   return (
     <ControlledEditor editorDidMount={handleEditorDidMount} {...editorConfig} />
